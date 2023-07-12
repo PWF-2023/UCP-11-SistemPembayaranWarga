27e6d83e3a57b85dc42b61919f09ae06bbffd8c8
enjoyghozi <adamghosi03@gmail.com>
2023-07-12 15:18:28 +0700

    feat(web) : feat landingpage template

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -21,8 +21,10 @@
     </head>
     <body class="font-sans text-gray-900 antialiased  bg-gray-100 dark:bg-gray-900">
         @include('layouts.guest-navigation')
-        <div class="sm:px-0 sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            {{ $slot }}
+        <div class="min-h-screen  px-4 sm:px-0 flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+            <div class="w-full sm:max-w-sm mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg rounded-lg">
+                {{ $slot }}
+            </div>
         </div>
     </body>
 </html>
