af04f7d6cebb62cf67d64fb7e18e29c5f35fd3b0
HabibSantoso <habib.santoso.ft18@mail.umy.ac.id>
2023-07-12 16:31:00 +0700

    add user habib

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -41,6 +41,15 @@ class DatabaseSeeder extends Seeder
                 'remember_token' => Str::random(10),
             ]
         );
+        User::create(
+            [
+                'name' => 'Habib Santoso',
+                'email' => 'habib.santosoti@gmail.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+            ]
+        );
         User::factory(100)->create();
         // Transaction::factory(100)->create();
         // Bill::factory(100)->create();
