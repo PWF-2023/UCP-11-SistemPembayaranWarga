bfb814af5ec83965ad1b3f285968d88596de42e5
HabibSantoso <habib.santoso.ft18@mail.umy.ac.id>
2023-07-13 20:37:13 +0700

    Update relasi user > bill menjadi m to m

--- database/migrations/2023_06_28_121205_create_bills_table.php
+++ database/migrations/2023_06_28_121205_create_bills_table.php
@@ -13,12 +13,10 @@ return new class extends Migration
     {
         Schema::create('bills', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->foreignUuid(column: 'user_id')->constrained(column: 'id')->on('users')->onDelete('cascade');
+            // $table->foreignUuid(column: 'user_id')->constrained(column: 'id')->on('users')->onDelete('cascade');
             $table->string('type', 255);
             $table->datetime('date_bill');
             $table->decimal('nominal', 10);
-            $table->boolean('is_pay')->nullable()->default(false);
-            $table->boolean('is_late')->nullable()->default(false);
             $table->timestamps();
         });
     }
